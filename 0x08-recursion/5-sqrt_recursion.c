1a21c2563b4ddbcfd00967542dfac6caca0ac157
nader777 <naderfekry3@gmail.com>
2023-10-25 14:55:36 -0700

    fixes

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * sqrtME - returns the natural square root of a number
+ * @n: test number
+ * @x: number to find the square root of
+ * Return: the square root of @n
+ */
+int sqrtME(int n, int x)
+{
+if (n > x / 2)
+return (-1);
+else if (n * n == x)
+return (n);
+return (sqrtME(n + 1, x));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: test number
+ * Return: the square root of @n
+ */
+int _sqrt_recursion(int n)
+{
+if (n == 0 || n == 1)
+return (n);
+return (sqrtME(0, n));
+}
