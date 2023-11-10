2d2434001a0dcb7e82cf5c4a12e7eff4e42bc099
nader777 <naderfekry3@gmail.com>
2023-11-10 06:49:11 -0800

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,22 +3,21 @@
 /**
  * main - print out the numbers from 0 to 9 with commas and spaces
  * Return: 0
- * Hasan Ragab
  */
 int main(void)
 {
-	int num;
+	int num1;
 	int num2;
 
-	for (num = 48; num < 58; num++)
+	for (num1 = 48; num1 < 58; num1++)
 	{
-		for (num2 = num + 1; num2 < 58; num2++)
+		for (num2 = num1 + 1; num2 < 58; num2++)
 		{
-			putchar(num);
+			putchar(num1);
 			putchar(num2);
-			if (num == '8' && num2 == '9')
+			if (num1 == '8' && num2 == '9')
 			{
-				continue;
+				break;
 			}
 			putchar(',');
 			putchar(' ');
