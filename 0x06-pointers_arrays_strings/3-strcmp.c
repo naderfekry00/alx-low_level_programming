0d151a92f012d620e56fe6020a566a104ab508e3
nader777 <naderfekry3@gmail.com>
2024-03-26 14:53:27 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,22 +5,28 @@
  * @s1: The first string to be compared.
  * @s2: The second string to be compared.
  *
- * Return: An integer less than, equal to, or greater than zero if s1 is found,
- * respectively, to be less than, to match, or be greater than s2.
+ * Return: zero if every character in s1 equal in AScII
+ * value to every character in s2(or vice versa), else return
+ * (1)if a character in s1 is different from s2 and its value
+ * is bigger than the one in s2, or (-1) if its value is less
+ *
  */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
+	int ret_urn;
 
-	while (*s1)
+	while (*s1 || *s2)/*loope continue if s2 longer than s2 like(Hello)&(Hello!)*/
 	{
-		if (*s1 != *s2)
+		if (*s1 == *s2)
 		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
-			break;
+			s1++;
+			s2++;
 		}
-		s1++;
-		s2++;
+		else if (*s1 != *s2)
+		{
+			ret_urn = ((int)*s2 - (int)*s2);/*(converting)charcters to value in ASCII*/
+			return (ret_urn);
+		}
 	}
-	return (equal);
+	return (0);
 }
